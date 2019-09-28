641ca33e55084b8b9f8db3b5e58dcb22d0b98e51
pbaral <55326286+pbaral@users.noreply.github.com>
2019-09-28 11:21:48 -0500

    Initial commit

---
+++ practiceVariables.py
@@ -0,0 +1,95 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+
+a = 4
+a = 3.45
+a = "string"
+a = True
+a = [4, 3.45, "string", True]
+print(a[2])
+
+
+name = "Pawan"
+print(name)
+
+"INTEGERS"
+
+var1 = 5
+print(var1)
+var2 = 10
+print(var2)
+var3 = var2
+print(var3)
+x = 24
+print(x)
+y = 100
+print(100)
+
+"FLOATS"
+
+z = 1.1
+print(z)
+a = 2.75
+print(a)
+c = 100.1
+print(c)
+d = 12.3
+print(d)
+
+"STRINGS"
+
+name = "Pawan"
+print(name)
+e = "p"
+print(e)
+f = "n"
+print(f)
+g = "chicken"
+print(g)
+h = name[0] + name[1] + name[2]
+print(h)
+
+"BOOLEANS"
+
+done = False
+print(done)
+smart = True
+print(smart)
+i = done
+print(i)
+j = False
+print(j)
+almostDone = True
+print(almostDone)
+
+"LIST"
+
+listInt = [5, 10, var2, 24, 100]
+print(listInt[2])
+listString = ["Pawan", "p", "n", "chicken",]
+print(listString[3])
+listBoolean = [False, True, done, False, True]
+print(listBoolean[0:4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
